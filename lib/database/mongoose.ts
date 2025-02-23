4a63054441aaa2e209a078e704ee784ea040829f
dev <techdep205@gmail.com>
2025-02-23 18:43:18 +0530

    commit

--- lib/database/mongoose.ts
+++ lib/database/mongoose.ts
@@ -1,32 +1,36 @@
-import mongoose, { Mongoose } from 'mongoose';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import mongoose from 'mongoose';
 
 const MONGODB_URL = process.env.MONGODB_URL;
 
 interface MongooseConnection {
-  conn: Mongoose | null;
-  promise: Promise<Mongoose> | null;
+  conn: mongoose.Mongoose | null;
+  promise: Promise<mongoose.Mongoose> | null;
 }
 
-let cached: MongooseConnection = (global as any).mongoose
+let cached: MongooseConnection = (global as unknown as { mongoose: MongooseConnection }).mongoose;
 
-if(!cached) {
-  cached = (global as any).mongoose = { 
-    conn: null, promise: null 
-  }
+if (!cached) {
+  cached = (global as unknown as { mongoose: MongooseConnection }).mongoose = {
+    conn: null,
+    promise: null,
+  };
 }
 
 export const connectToDatabase = async () => {
-  if(cached.conn) return cached.conn;
+  if (cached.conn) return cached.conn;
 
-  if(!MONGODB_URL) throw new Error('Missing MONGODB_URL');
+  if (!MONGODB_URL) throw new Error('Missing MONGODB_URL');
 
-  cached.promise = 
-    cached.promise || 
-    mongoose.connect(MONGODB_URL, { 
-      dbName: 'imaginify', bufferCommands: false 
-    })
+  cached.promise =
+    cached.promise ||
+    mongoose.connect(MONGODB_URL, {
+      dbName: 'imaginify',
+      bufferCommands: false,
+    });
 
   cached.conn = await cached.promise;
 
   return cached.conn;
-}
+};
--- lib/utils.ts
+++ lib/utils.ts
@@ -85,11 +85,11 @@ export function removeKeysFromQuery({
 }
 
 // DEBOUNCE
-export const debounce = (func: (...args: any[]) => void, delay: number) => {
+export const debounce = (func: (...args: unknown[]) => void, delay: number) => {
   let timeoutId: NodeJS.Timeout | null;
-  return (...args: any[]) => {
+  return (...args: unknown[]) => {
     if (timeoutId) clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func.apply(null, args), delay);
+    timeoutId = setTimeout(() => func(...args), delay);
   };
 };
 
@@ -131,8 +131,8 @@ export const download = (url: string, filename: string) => {
 };
 
 // DEEP MERGE OBJECTS
-export const deepMergeObjects = (obj1: any, obj2: any) => {
-  if(obj2 === null || obj2 === undefined) {
+export const deepMergeObjects = (obj1: Record<string, unknown>, obj2: Record<string, unknown>) => {
+  if (obj2 === null || obj2 === undefined) {
     return obj1;
   }
 
@@ -146,7 +146,7 @@ export const deepMergeObjects = (obj1: any, obj2: any) => {
         obj2[key] &&
         typeof obj2[key] === "object"
       ) {
-        output[key] = deepMergeObjects(obj1[key], obj2[key]);
+        output[key] = deepMergeObjects(obj1[key] as Record<string, unknown>, obj2[key] as Record<string, unknown>);
       } else {
         output[key] = obj1[key];
       }
